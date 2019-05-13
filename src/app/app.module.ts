5dc7ff2ed3747e99517b0348b8363728959844a1
Esteban Sanchez <41240399+esanchezvz@users.noreply.github.com>
2019-05-13 02:29:06 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Modulos
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { ProgressComponent } from './components/pages/progress/progress.component';
+import { Graficas1Component } from './components/pages/graficas1/graficas1.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './components/shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { RegisterComponent } from './components/login/register.component';
+
+// Servicios
+import { SharedService } from './services/shared.service';
+import { SidebarService } from './services/sidebar.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
